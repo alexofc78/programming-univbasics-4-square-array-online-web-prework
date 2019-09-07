c04831f72c9785fa2dab55ee00850096eaa33ebc
ALEXANDRA OLIVEROS <alexofc@gmail.com>
2019-09-07 16:42:42 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,8 +1,8 @@
 def square_array(array)
   new_array = []
-  counter = 2
+  counter = 0
   while counter < array.length do
     new_array << array[counter] ** 2
-    counter += 6
+    counter += 1
   end
   
